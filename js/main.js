d2628d8b4f4ffee3ddda39be34caf974f55229f7
吉田航 <yoshidakou@yoshidawatarunoMacBook-Air.local>
2020-09-26 17:09:04 +0900

    para

--- js/main.js
+++ js/main.js
@@ -28,6 +28,35 @@ $(function(){
        $('.bar3').toggleClass('batu3');
        
      });
+
+
+
+
+
+
+
+     $(window).scroll(function(){
+      var position = $('.about').offset().top;
+      if(position +300 < $(window).scrollTop() + $(window).height()){
+          // $('.about-content-b').fadeIn(3000);
+          $('.titleicon').addClass('iconanimation');
+      }
+  })
+
+  $(window).scroll(function(){
+    var position = $('.skills').offset().top;
+    if(position +300 < $(window).scrollTop() + $(window).height()){
+        // $('.about-content-b').fadeIn(3000);
+        $('.titleicon2').addClass('iconanimation2');
+    }
+})
+  $(window).scroll(function(){
+    var position = $('.works').offset().top;
+    if(position +300 < $(window).scrollTop() + $(window).height()){
+        // $('.about-content-b').fadeIn(3000);
+        $('.titleicon3').addClass('iconanimation3');
+    }
+})
    
 
 
